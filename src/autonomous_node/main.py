7818051f8c6cdaa4cb894f4b9eb7d6ccd61ad381
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-01-22 07:02:42 +0000

    Add python publish config msg

--- src/autonomous_node/main.py
+++ src/autonomous_node/main.py
@@ -6,15 +6,29 @@ from threading import Thread
 
 from frc_robot_utilities_py_node.frc_robot_utilities_py import *
 from frc_robot_utilities_py_node.RobotStatusHelperPy import RobotStatusHelperPy, Alliance, RobotMode
-
+from ck_ros_msgs_node.msg import Autonomous_Configuration
 
 def ros_func():
     global hmi_updates
     global robot_status
 
-    rate = rospy.Rate(20)
-    # Put your code in the appropriate sections in this if statement/while loop
+    auto_configuration_publisher = rospy.Publisher(name="AutonomousConfiguration", data_class=Autonomous_Configuration, queue_size=50, tcp_nodelay=True)
+    autonomous_configuration_options = Autonomous_Configuration()
+    autonomous_configuration_options.autonomous_options = ["Climb", "Score Two + Climb"]
+    autonomous_configuration_options.game_pieces = ["Cone", "Cube"]
+    autonomous_configuration_options.starting_positions = ["Wall", "Middle", "Loading Side"]
+
+    config_publish_counter : int = 1
+
+    rate = rospy.Rate(50)
     while not rospy.is_shutdown():
+        #Publish auto configuration once every two seconds
+        if (config_publish_counter % 100 == 0):
+            auto_configuration_publisher.publish(autonomous_configuration_options)
+            config_publish_counter = 1
+        else:
+            config_publish_counter += 1
+
         if robot_status.get_mode() == RobotMode.AUTONOMOUS:
             pass
         elif robot_status.get_mode() == RobotMode.TELEOP:
