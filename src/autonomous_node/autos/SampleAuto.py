2574d597129348620f8ff2b62af46a5e4a338193
Robert Hilton <robert.a.hilton.jr@gmail.com>
2023-02-15 17:10:10 +0000

    Expand autonomous example

--- src/autonomous_node/autos/SampleAuto.py
+++ src/autonomous_node/autos/SampleAuto.py
@@ -1,9 +1,33 @@
 from autonomous_node.autos.AutoBase import AutoBase
 from actions_node.default_actions.SeriesAction import SeriesAction
+from actions_node.default_actions.ParallelAction import ParallelAction
 from actions_node.default_actions.DriveTrajectoryAction import DriveTrajectoryAction
+from actions_node.game_specific_actions.MoveArmAction import MoveArmAction, ArmPosition
+from actions_node.game_specific_actions.IntakeAction import IntakeAction
+from actions_node.game_specific_actions.AutomatedActions import *
 
 class SampleAuto(AutoBase):
     def getAction(self) -> SeriesAction:
         return SeriesAction([
-            DriveTrajectoryAction("correct_start", True)
+            ParallelAction([
+                DriveTrajectoryAction("correct_start", True),
+                SeriesAction([
+                    GroundAction(True),
+                    IntakeAction(False)
+                ])
+            ]),
+            IntakeAction(False, 0.5),
+            ParallelAction([
+                InRobotAction(),
+                DriveTrajectoryAction("second_path")
+            ]),
+            ScoreCube(False),
+            InRobotAction(),
+            ParallelAction([
+                DriveTrajectoryAction("third_path", True),
+                SeriesAction([
+                    GroundAction(True),
+                    IntakeAction(False)
+                ])
+            ])
         ])
