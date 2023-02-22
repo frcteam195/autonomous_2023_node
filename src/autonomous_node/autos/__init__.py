adb22f00eab9a1591ab1c670cdf2aefe3a79cfdb
Macbook2 <frc195programming@gmail.com>
2023-02-21 21:39:19 -0500

    Added new auto

--- src/autonomous_node/autos/__init__.py
+++ src/autonomous_node/autos/__init__.py
@@ -4,6 +4,7 @@ from autonomous_node.autos.SimpleAuto import SimpleAuto
 from autonomous_node.autos.ToddCircle import ToddCircle
 from autonomous_node.autos.CorrectStart import CorrectStart
 from autonomous_node.autos.CubeLoadingTwoPiece import CubeLoadingTwoPiece
+from autonomous_node.autos.CubeMiddleOnePieceClimb import CubeMiddleOnePieceClimb
 
 
 class AutonomousNames(str, Enum):
@@ -14,6 +15,7 @@ class AutonomousNames(str, Enum):
     ToddCircle = "Test_Test_ToddCircle"
     CorrectStart = "Test_Test_CorrectStart"
     CubeLoadingTwoPiece = "Cube_Loading_TwoPiece"
+    CubeMiddleOnePieceClimb = "Cube_Middle_OnePieceClimb"
 
     def __str__(self) -> str:
         return str.__str__(self)
@@ -23,5 +25,6 @@ AUTONOMOUS_SELECTION_MAP = {
     AutonomousNames.SimpleAuto: SimpleAuto(),
     AutonomousNames.ToddCircle: ToddCircle(),
     AutonomousNames.CorrectStart: CorrectStart(),
-    AutonomousNames.CubeLoadingTwoPiece: CubeLoadingTwoPiece()
+    AutonomousNames.CubeLoadingTwoPiece: CubeLoadingTwoPiece(),
+    AutonomousNames.CubeMiddleOnePieceClimb: CubeMiddleOnePieceClimb()
 }
