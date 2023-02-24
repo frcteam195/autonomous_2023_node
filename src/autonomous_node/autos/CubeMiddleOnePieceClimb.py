cfddf52df33da961fc7c1c904c3f5e6d5b3d7bb3
Macbook2 <frc195programming@gmail.com>
2023-02-23 20:26:00 -0500

    Fixed Cube and climb auto

--- src/autonomous_node/autos/CubeMiddleOnePieceClimb.py
+++ src/autonomous_node/autos/CubeMiddleOnePieceClimb.py
@@ -2,10 +2,12 @@ from autonomous_node.autos.AutoBase import AutoBase, GamePiece, StartPosition
 
 from actions_node.default_actions.SeriesAction import SeriesAction
 from actions_node.default_actions.ParallelAction import ParallelAction
+from actions_node.default_actions.WaitUntilPercentCompletedTrajectoryAction import WaitUntilPercentCompletedTrajectoryAction
+from actions_node.default_actions.ResetPoseAction import ResetPoseAction
+
 
 from actions_node.game_specific_actions.IntakeAction import IntakeAction
 from actions_node.game_specific_actions.AutomatedActions import *
-from actions_node.default_actions.ResetPoseAction import ResetPoseAction
 
 from ck_ros_msgs_node.msg import Arm_Goal
 
@@ -23,13 +25,17 @@ class CubeMiddleOnePieceClimb(AutoBase):
         return SeriesAction([
             ResetPoseAction(self.get_unique_name()),
             ScoreCubeHigh(Arm_Goal.SIDE_BACK),
-            MoveArmAction(Arm_Goal.HOME, Arm_Goal.SIDE_FRONT),
-            MoveArmAction(Arm_Goal.GROUND_CONE, Arm_Goal.SIDE_FRONT),
-            IntakeAction(True),
-            self.trajectory_iterator.get_next_trajectory_action(),
-            IntakeAction(True, 0.0),
+            ParallelAction([
+                self.trajectory_iterator.get_next_trajectory_action(),
+                MoveArmAction(Arm_Goal.GROUND_CONE, Arm_Goal.SIDE_FRONT),
+                SeriesAction([
+                    WaitUntilPercentCompletedTrajectoryAction(0, 0.75),
+                    IntakeAction(True),
+                ])
+            ]),
             ParallelAction([
                 MoveArmAction(Arm_Goal.HOME, Arm_Goal.SIDE_FRONT),
-                self.trajectory_iterator.get_next_trajectory_action()
+                self.trajectory_iterator.get_next_trajectory_action(),
+                StopIntakeAction(True)
             ]),
         ])
