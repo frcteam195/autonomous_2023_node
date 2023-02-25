b01b45c9a5805e86a79ea306ceefad23f01caa41
Mac #1 <frc195programming@gmail.com>
2023-02-25 16:32:46 -0500

    fixed stuff

--- src/autonomous_node/autos/CubeWallTwoPiece.py
+++ src/autonomous_node/autos/CubeWallTwoPiece.py
@@ -23,21 +23,25 @@ class CubeWallTwoPiece(AutoBase):
     def get_action(self) -> SeriesAction:
         return SeriesAction([
             ResetPoseAction(self.get_unique_name()),
+            StopIntakeAction(True),
             ScoreCubeHigh(Arm_Goal.SIDE_BACK),
-            #MoveArmAction(Arm_Goal.HOME, Arm_Goal.SIDE_FRONT),
-            MoveArmAction(Arm_Goal.GROUND_CONE, Arm_Goal.SIDE_FRONT),
-            IntakeAction(True),
-            self.trajectory_iterator.get_next_trajectory_action(),
-            IntakeAction(True, 0.0),
+            ParallelAction([    
+                self.trajectory_iterator.get_next_trajectory_action(),
+                MoveArmAction(Arm_Goal.GROUND_CONE, Arm_Goal.SIDE_FRONT),
+                SeriesAction([
+                    WaitUntilPercentCompletedTrajectoryAction(0, 0.75),
+                    IntakeAction(True, 1.0)
+                ])
+            ]),
             ParallelAction([
-                MoveArmAction(Arm_Goal.HOME, Arm_Goal.SIDE_FRONT),
+                StopIntakeAction(True),
                 self.trajectory_iterator.get_next_trajectory_action(),
                 SeriesAction([
-                    WaitUntilPercentCompletedTrajectoryAction(1, 0.5),
                     MoveArmAction(Arm_Goal.PRE_SCORE, Arm_Goal.SIDE_BACK),
-                    ScoreConeHigh(Arm_Goal.SIDE_BACK, Arm_Goal.WRIST_180)
-                ]),
-                StopIntakeAction(False)
+                    WaitUntilPercentCompletedTrajectoryAction(1, 0.75),
+                    MoveArmAction(Arm_Goal.HIGH_CONE, Arm_Goal.SIDE_BACK, Arm_Goal.WRIST_180)         
+                ])
             ]),
             
+            StopIntakeAction(False)
         ])
