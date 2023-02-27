e623368a3c59880059b25fc87690f75853ad14ec
frc195programming <frc195programming@gmail.com>
2023-02-27 01:45:32 +0000

    added cone loading 3 piece mid link

--- src/autonomous_node/autos/__init__.py
+++ src/autonomous_node/autos/__init__.py
@@ -3,6 +3,7 @@ from enum import Enum
 from autonomous_node.autos.CubeLoadingThreePiece import CubeLoadingThreePiece
 from autonomous_node.autos.ConeLoadingThreePieceClimb import ConeLoadingThreePieceClimb
 from autonomous_node.autos.ConeLoadingThreePiece import ConeLoadingThreePiece
+from autonomous_node.autos.ConeLoadingThreePieceMidLink import ConeLoadingThreePieceMidLink
 
 class AutonomousNames(str, Enum):
     """
@@ -11,6 +12,7 @@ class AutonomousNames(str, Enum):
     CubeLoadingThreePiece = "Cube_Loading_ThreePiece"
     ConeLoadingThreePieceClimb = "Cone_Loading_ThreePieceClimb"
     ConeLoadingThreePiece = "Cone_Loading_ThreePiece"
+    ConeLoadingThreePieceMidLink = "Cone_Loading_ThreePieceMidLink"
 
     def __str__(self) -> str:
         return str.__str__(self)
@@ -19,5 +21,6 @@ class AutonomousNames(str, Enum):
 AUTONOMOUS_SELECTION_MAP = {
     AutonomousNames.CubeLoadingThreePiece: CubeLoadingThreePiece(),
     AutonomousNames.ConeLoadingThreePieceClimb: ConeLoadingThreePieceClimb(),
-    AutonomousNames.ConeLoadingThreePiece: ConeLoadingThreePiece()
+    AutonomousNames.ConeLoadingThreePiece: ConeLoadingThreePiece(),
+    AutonomousNames.ConeLoadingThreePieceMidLink: ConeLoadingThreePieceMidLink()
 }
