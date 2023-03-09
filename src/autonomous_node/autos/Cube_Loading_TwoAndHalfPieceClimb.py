fb2a2805218e685e4de01f10c881e7b1934dadb9
Mac #1 <frc195programming@gmail.com>
2023-03-08 20:03:36 -0500

    auto change

--- src/autonomous_node/autos/Cube_Loading_TwoAndHalfPieceClimb.py
+++ src/autonomous_node/autos/Cube_Loading_TwoAndHalfPieceClimb.py
@@ -6,6 +6,8 @@ from actions_node.default_actions.ParallelAction import ParallelAction
 
 from actions_node.game_specific_actions.IntakeAction import IntakeAction
 from actions_node.game_specific_actions.AutomatedActions import *
+from actions_node.game_specific_actions.LaunchAction import LaunchAction
+
 
 from ck_ros_msgs_node.msg import Arm_Goal
 from actions_node.default_actions.ResetPoseAction import ResetPoseAction
@@ -24,38 +26,37 @@ class CubeLoadingTwoAndHalfPieceClimb(AutoBase):
     def get_action(self) -> SeriesAction:
         return SeriesAction([
             ResetPoseAction(self.get_unique_name()),
-            MoveArmAction(Arm_Goal.LOW_SCORE, Arm_Goal.SIDE_BACK),
+            MoveArmAction(Arm_Goal.LOW_SCORE, Arm_Goal.SIDE_FRONT),
+            OuttakeAction(False, 0.2),
             ParallelAction([
                 self.trajectory_iterator.get_next_trajectory_action(),
-                MoveArmAction(Arm_Goal.GROUND_CUBE, Arm_Goal.SIDE_FRONT),
+                MoveArmAction(Arm_Goal.GROUND_CUBE, Arm_Goal.SIDE_BACK),
                 SeriesAction([
-                    WaitUntilPercentCompletedTrajectoryAction(0, 0.5),
+                    WaitUntilPercentCompletedTrajectoryAction(0, 0.75),
                     IntakeAction(False)
                 ])
             ]),
             ParallelAction([
                 self.trajectory_iterator.get_next_trajectory_action(),
                 StopIntakeAction(False),
-                MoveArmAction(Arm_Goal.LOW_SCORE, Arm_Goal.SIDE_BACK),
+                MoveArmAction(Arm_Goal.LOW_SCORE, Arm_Goal.SIDE_FRONT)
             ]),
-            OuttakeAction(False, 0.1),
-            StopIntakeAction(False),
+            OuttakeAction(False, 0.2),
             ParallelAction([
+                StopIntakeAction(False),
                 self.trajectory_iterator.get_next_trajectory_action(),
-                MoveArmAction(Arm_Goal.GROUND_CUBE, Arm_Goal.SIDE_FRONT),
+                MoveArmAction(Arm_Goal.GROUND_CUBE, Arm_Goal.SIDE_BACK),
                 SeriesAction([
-                    WaitUntilPercentCompletedTrajectoryAction(2, 0.5),
+                    WaitUntilPercentCompletedTrajectoryAction(2, 0.75),
                     IntakeAction(False)
-            ]),
+            ])
             ]),
             ParallelAction([
+                StopIntakeAction(False),
                 self.trajectory_iterator.get_next_trajectory_action(),
-                SeriesAction([
-                    WaitUntilPercentCompletedTrajectoryAction(3, 0.15),
-                    MoveArmAction(Arm_Goal.SPORT_MODE, Arm_Goal.SIDE_FRONT)
-                ])
+                MoveArmAction(Arm_Goal.SPORT_MODE, Arm_Goal.SIDE_BACK)
             ]),
-            AutoBalanceAction(BalanceDirection.ROLL, 90.0)
+            AutoBalanceAction(BalanceDirection.ROLL, 270.0)
         ]) 
 
     
