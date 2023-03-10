87a0635efeb71032a4d8e9a160554367a7f47c92
Mac #1 <frc195programming@gmail.com>
2023-03-10 22:41:12 +0000

    auto

--- src/autonomous_node/autos/CubeMiddleOnePieceClimb.py
+++ src/autonomous_node/autos/CubeMiddleOnePieceClimb.py
@@ -17,16 +17,17 @@ class CubeMiddleOnePieceClimb(AutoBase):
     Scores one in Cube Node and climbs
     """
     def __init__(self) -> None:
-        super().__init__(display_name="2MidClimb",
-                         game_piece=GamePiece.Cone,
-                         start_position=StartPosition.Loading,
-                         expected_trajectory_count=3)
+        super().__init__(display_name="OnePieceClimb",
+                         game_piece=GamePiece.Cube,
+                         start_position=StartPosition.Middle,
+                         expected_trajectory_count=1)
 
     def get_action(self) -> SeriesAction:
         return SeriesAction([
             ResetPoseAction(self.get_unique_name()),
             MoveArmAction(Arm_Goal.PRE_SCORE, Arm_Goal.SIDE_FRONT),
             MoveArmAction(Arm_Goal.HIGH_CUBE, Arm_Goal.SIDE_FRONT),
+            OuttakeAction(False, 0.2),
             StopIntakeAction(False),
             ParallelAction([
                 self.trajectory_iterator.get_next_trajectory_action(),
