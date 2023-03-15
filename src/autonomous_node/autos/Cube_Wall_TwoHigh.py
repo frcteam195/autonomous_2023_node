49502fb2c72df9dcbee35abb0988958efd0d426a
Tom <TSLourenco@WPI.edu>
2023-03-15 03:00:18 +0000

    Tweaked two high on the wall.

--- src/autonomous_node/autos/Cube_Wall_TwoHigh.py
+++ src/autonomous_node/autos/Cube_Wall_TwoHigh.py
@@ -6,7 +6,6 @@ from actions_node.default_actions.ParallelAction import ParallelAction
 from actions_node.default_actions.WaitUntilPercentCompletedTrajectoryAction import WaitUntilPercentCompletedTrajectoryAction
 
 from actions_node.game_specific_actions.AutomatedActions import *
-from actions_node.game_specific_actions.IntakeAction import IntakeAction
 from actions_node.game_specific_actions.LaunchAction import LaunchAction
 
 from ck_ros_msgs_node.msg import Arm_Goal
@@ -20,7 +19,7 @@ class Cube_Wall_TwoHigh(AutoBase):
         super().__init__(display_name="TwoHigh",
                          game_piece=GamePiece.Cube,
                          start_position=StartPosition.Wall,
-                         expected_trajectory_count=3)
+                         expected_trajectory_count=2)
 
     def get_action(self) -> SeriesAction:
         return SeriesAction([
@@ -30,10 +29,8 @@ class Cube_Wall_TwoHigh(AutoBase):
             LaunchAction(False, 0.85, 0.1),
             ParallelAction([
                 self.trajectory_iterator.get_next_trajectory_action(),
-                MoveArmAction(Arm_Goal.HOME, Arm_Goal.SIDE_BACK, Arm_Goal.WRIST_180)
+                MoveArmAction(Arm_Goal.PRE_DEAD_CONE, Arm_Goal.SIDE_BACK, Arm_Goal.WRIST_180)
             ]),
-            MoveArmAction(Arm_Goal.PRE_DEAD_CONE, Arm_Goal.SIDE_BACK, Arm_Goal.WRIST_180),
-            self.trajectory_iterator.get_next_trajectory_action(),
             IntakeDeadCone(Arm_Goal.SIDE_FRONT, Arm_Goal.WRIST_180),
             ParallelAction([
                 self.trajectory_iterator.get_next_trajectory_action(),
@@ -44,7 +41,6 @@ class Cube_Wall_TwoHigh(AutoBase):
                     MoveArmAction(Arm_Goal.PRE_SCORE, Arm_Goal.SIDE_FRONT, Arm_Goal.WRIST_ZERO)
                 ])
             ]),
-            MoveArmAction(Arm_Goal.HIGH_CONE, Arm_Goal.SIDE_FRONT, Arm_Goal.WRIST_ZERO),
-            StopIntakeAction(False, 0.15),
+            ScoreHighconeAction(Arm_Goal.SIDE_FRONT, Arm_Goal.WRIST_ZERO),
             MoveArmAction(Arm_Goal.HOME, Arm_Goal.SIDE_FRONT, Arm_Goal.WRIST_ZERO)
         ])
